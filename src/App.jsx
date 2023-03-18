7e17a3a4ac6a97a7d5026a2de55c5d6fad8652a3
mohammed3312 <mohammed_1818@outlook.com>
2023-03-18 16:24:52 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Regester from './Regester'
+
+const App = () => {
+  return (
+    <main className='App'>
+      <Regester />
+    </main>
+  )
+}
+
+export default App
